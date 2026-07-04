fb4bf58f1346c49f646a3a3362eb8cebba190be2
Leticia Cardoso <leticiaapc06@gmail.com>
2026-07-04 11:15:09 -0300

    add exercicios java orientado a objeto

---
+++ Jogo.java
@@ -0,0 +1,6 @@
+public class Jogo{
+    public String nome;
+    public double preco;
+    public int parcelamentoDisponivel ;
+    public double valorParcela;
+}
---
+++ Principal.java
@@ -0,0 +1,8 @@
+public class Principal{
+
+public static void main(String[] args){
+    FabricanteExecutor executor = new FabricanteExecutor();
+    executor.excutar();
+}
+} 
+
